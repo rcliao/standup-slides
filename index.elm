51805f63408ec40661420394b65f225a5bd7901a
Eric Liao <eric@edlio.com>
2018-05-24 18:56:46 -0700

    feat: add sample elm program

---
+++ index.elm
@@ -0,0 +1,54 @@
+module Main exposing (..)
+
+import Html exposing (Html, button, div, text, input)
+import Html.Attributes exposing (placeholder, type_)
+import Html.Events exposing (onInput)
+
+
+main =
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
+
+
+
+-- Model
+
+
+type alias Model =
+    { name : String
+    }
+
+
+model : Model
+model =
+    Model "Eric"
+
+
+
+-- Update
+
+
+type Msg
+    = Name String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Name name ->
+            { model | name = name }
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input [ type_ "text", placeholder "Name", onInput Name ] []
+        , div [] [ text model.name ]
+        ]
