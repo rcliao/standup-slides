b4aa7930f869be6c6f14064b9a0e9596fb091aae
Eric Liao <rcliao01@gmail.com>
2018-06-01 08:17:31 -0700

    feat: add editor rendering

--- src/view.js
+++ src/view.js
@@ -3,16 +3,15 @@ import * as SimpleMDE from 'simplemde';
 export function setupEditor (app) {
     let editor;
     app.ports.renderEditor.subscribe(function(view) {
-        console.log(view);
         if (view === 'Notes') {
-            setTimeout(function() {
+            requestAnimationFrame(function() {
                 const editorDOM = document.querySelector('#note_editor');
-                console.log(editorDOM);
                 if (editorDOM) {
                     editor = new SimpleMDE({ element: editorDOM });
+                } else {
+                    console.log('Editor DOM is not found. Cannot add editor widget');
                 }
-                console.log(editor);
-            }, 500);
+            });
         } else {
             editor.toTextArea();
             editor = null;
